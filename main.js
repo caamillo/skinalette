2d74afcac7608980bb8f154ac3dbc894fb58d821
Diasky <diaskygotah@gmail.com>
2022-05-09 17:52:41 +0200

    init

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const morgan = require('morgan')
+
+// Routers
+
+
+;(async () => {
+    const app = express()
+    app.use(morgan('tiny'))
+    app.set('trust proxy', 1)
+
+    app.use(express.static('public'))
+    app.use(express.urlencoded({ extended:true }))
+
+    app.set('view engine','ejs')
+
+    app.get('/', (req,res) => {
+        res.render('index')
+    })
+
+    app.listen(3000, () => {
+        console.log("Server is running on http://localhost:3000")
+    })
+})()
