cdda446c72c98ec5b1df16d131f24648bf26ce88
Diasky <diaskygotah@gmail.com>
2022-05-10 21:01:54 +0200

    getpixels and colors

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const express = require("express")
 const morgan = require('morgan')
+const getPixels = require('get-pixels')
 
-// Routers
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+  
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 
 ;(async () => {
     const app = express()
@@ -15,6 +23,28 @@ const morgan = require('morgan')
 
     app.get('/', (req,res) => {
         res.render('index')
+    })
+
+    app.get('/test', async (req,res) => {
+        const colors = []
+        getPixels('./public/img/test.png', (err,pixels) => {
+            if (err) return console.log(err)
+            for(let i = 0; i < pixels.shape[0]; i++) {
+                for(let j = 0; j < pixels.shape[1]; j++) {
+                    const color = []
+                    for(let k = 0; k < 4; k++) {
+                        color.push(pixels.data[(i * pixels.shape[1] + j) * 4 + k])
+                    }
+                    const hex = rgbToHex(
+                        color[0],
+                        color[1],
+                        color[2]
+                    )
+                    if (!(colors.includes(hex))) colors.push(hex)
+                }
+            }
+            res.json(colors)
+        })
     })
 
     app.listen(3000, () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
       lg: '976px',
       xl: '1440px'
     },
+    colors: {
+      'blurple': '#736CED',
+      'lightblurple': '#9F9FED',
+      'magnolia': '#F6EEFB',
+      'snow': '#FEF9FF',
+      'champagnepink': '#F2DFD7'
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
