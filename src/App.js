cd34b639937b1070822ada02752a5ce30f561d75
Diasky <diaskygotah@gmail.com>
2022-05-14 23:40:07 +0200

    some css

--- src/App.js
+++ src/App.js
@@ -62,23 +62,43 @@ function App() {
 
     return (
         <div id="container">
-            <nav className="relative container mx-auto p-6">
+            <nav className="absolute left-0 right-0 container mx-auto p-6">
                 <div className="flex items-center justify-between">
                     <div className="text-3xl text-blurple font-radiocanada font-semibold">Skinalette</div>
                     <div className="hidden space-x-6 text-blurple font-radiocanada md:flex md:justify-end">
                         <a className="hover:text-lightblurple" href="#">Home</a>
-                        <a className="hover:text-lightblurple" href="#">Changer</a>
+                        <a className="hover:text-lightblurple" href="#">About</a>
                     </div>
                 </div>
             </nav>
-            <div className="w-full border-t" style={{"borderColor":"#736CED"}}></div>
-            <section id="home">
-                <div id="test">
-                    <Skinview3d skinUrl={skin} height = "500" width = "500" />
+            <section id="home" className='flex items-center justify-center w-screen h-screen'>
+                <div className="cards md:space-x-6">
+                    <div id="fromcard" className='flex border-2 rounded border-blurple md:inline-block'>
+                        <div className="content flex items-center">
+                            <div className="avatar">
+                                <Skinview3d skinUrl={skin} height = "300" width = "300" />
+                            </div>
+                            <div className="desc mb-20 mr-5">
+                                <div className="title mb-5 text-blurple font-semibold font-radiocanada text-3xl">
+                                    Imported Skin
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="tocard" className='flex border-2 rounded border-blurple md:inline-block'>
+                        <div className="content flex items-center">
+                            <div className="avatar">
+                                <Skinview3d skinUrl={skin} height = "300" width = "300" />
+                            </div>
+                            <div className="desc  mb-20">
+                                <div className="title">
+                                    Ciao
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
-            <section id="changer"></section>
-            <section id="about"></section>
         </div>
     );
 }
