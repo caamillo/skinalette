7a84eba6d68516680e0b31fbc5f3f34c25ecf856
Diasky <diaskygotah@gmail.com>
2022-05-15 15:00:31 +0200

    responsive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import { useEffect, useState } from 'react';
 
 // Media
-import skin from './img/test.png'
+import inputSkin from './img/input.png'
+import outputSkin from './img/output.png'
 
 // NPM
 import pixels from 'image-pixels'
+import Skinview3d from 'react-skinview3d'
+import replaceColor from 'replace-color'
+
+// Components
 import Color from './components/Color'
-import Skinview3d from "react-skinview3d"
 
 // Tailwind
 import './tailwind/compiled.css'
@@ -28,7 +32,7 @@ function App() {
 
     useEffect(() => {
         async function getPixels(){
-            const image = await pixels(skin)
+            const image = await pixels(inputSkin)
             for(let i = 0; i < image.height; i++){
                 pixelLoop:
                 for(let j = 0; j < image.width; j++){
@@ -66,34 +70,30 @@ function App() {
                 <div className="flex items-center justify-between">
                     <div className="text-3xl text-blurple font-radiocanada font-semibold">Skinalette</div>
                     <div className="hidden space-x-6 text-blurple font-radiocanada md:flex md:justify-end">
-                        <a className="hover:text-lightblurple" href="#">Home</a>
+                        <a className="hover:text-lightblurple selected" href="#">Home</a>
                         <a className="hover:text-lightblurple" href="#">About</a>
                     </div>
                 </div>
             </nav>
             <section id="home" className='flex items-center justify-center w-screen h-screen'>
-                <div className="cards md:space-x-6">
-                    <div id="fromcard" className='flex border-2 rounded border-blurple md:inline-block'>
-                        <div className="content flex items-center">
-                            <div className="avatar">
-                                <Skinview3d skinUrl={skin} height = "300" width = "300" />
+                <div className="cards space-y-6 w-[100vw] child:w-[80vw] child:md:w-auto md:w-auto md:space-y-0 md:space-x-6">
+                    <div id="fromcard" className='flex mx-auto border-2 rounded border-blurple child:w-[100vw] child:md:w-auto md:inline-block'>
+                        <div className="content flex items-center child:w-[100vw] child:md:w-auto">
+                            <div className="avatar child:w-[50vw] child:h-[50vw] child:md:w-[300px] child:md:h-auto">
+                                <Skinview3d skinUrl = { inputSkin } height = "300" width = "300" />
                             </div>
-                            <div className="desc mb-20 mr-5">
-                                <div className="title mb-5 text-blurple font-semibold font-radiocanada text-3xl">
-                                    Imported Skin
+                            <div className="desc mb-15">
+                                <div className="title text-blurple font-semibold font-radiocanada text-[6vw] md:text-3xl">
+                                    <span>Imported</span>
                                 </div>
+                                <button type="button" onclick="alert('Hello world!')">Click Me!</button>
                             </div>
                         </div>
                     </div>
-                    <div id="tocard" className='flex border-2 rounded border-blurple md:inline-block'>
+                    <div id="tocard" className='flex mx-auto justify-center border-2 rounded border-blurple md:inline-block'>
                         <div className="content flex items-center">
-                            <div className="avatar">
-                                <Skinview3d skinUrl={skin} height = "300" width = "300" />
-                            </div>
-                            <div className="desc  mb-20">
-                                <div className="title">
-                                    Ciao
-                                </div>
+                            <div className="avatar child:w-[50vw] child:h-[50vw] child:md:w-[300px] child:md:h-auto">
+                                <Skinview3d skinUrl = { outputSkin } height = "300" width = "300" />
                             </div>
                         </div>
                     </div>
