791c486145085b84ed2d6c8a87cba33ff6234742
Diasky <diaskygotah@gmail.com>
2022-05-29 11:58:46 +0200

    double-outline colors

--- src/components/Color.js
+++ src/components/Color.js
@@ -5,15 +5,25 @@ const Color = ({colorstart, id, colorChange}) => {
     const [color, setColor] = useState(colorstart)
 
     return(
-        <div
-        className="color"
-        style = {{
+        <div className="color-container" style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
             width: '50px',
-            height: '50px',
-            backgroundColor: color
-        }}
-        onClick = { () => colorChange(id, color, setColor) }
-        />
+            height: '50px'
+        }}>
+            <div
+            className="color"
+            style = {{
+                width: '100%',
+                height: '100%',
+                backgroundColor: color,
+                borderRadius: '0.375rem',
+                border: '2px solid #FEF9FF'
+            }}
+            onClick = { () => colorChange(id, color, setColor) }
+            />
+        </div>
     )
 }
 
